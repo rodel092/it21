ee24d8fa719514eacd1f5eed4fd266956f94b4ba
rodel092 <rodelsaligumba09@gmail.com>
2025-09-05 13:24:04 +0800

    Added lab-factorial

---
+++ factorial.js
@@ -0,0 +1,6 @@
+console.log("----------------");
+console.log("Factorial Application");
+console.log("1. Say hello");
+console.log("2. Factorial");
+console.log("3. Exit Application");
+console.log("----------------");
