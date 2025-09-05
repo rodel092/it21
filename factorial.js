b974a1e9530c10d7d8c2f8d564c3813f4fbed34a
rodel092 <rodelsaligumba09@gmail.com>
2025-09-05 13:39:35 +0800

    Added lab-factorial init

--- factorial.js
+++ factorial.js
@@ -11,3 +11,21 @@ console.log("1. Say hello");
 console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("----------------");
+
+rl.question("Enter your choice (1-3"); ", choice =>";{
+    console.log(choice);
+
+   if (choice === "1"){
+    console.log("Hello");
+   } else if (choice === "2"){
+    console.log("Factorial");
+   } else if (choice === "3"){
+    console.log("Exiting Application");
+   } else {
+    console.log("Invalid Choice. Please Try Again");
+   }
+
+
+
+}
+
