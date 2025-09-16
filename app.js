edf0ddcebd47b7d692e9b743c61cb8c5b132af79
rodel092 <rodelsaligumba09@gmail.com>
2025-09-17 07:34:53 +0800

    Added person info final

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("This is app.js");
+class Person{
+    name;
+    age;
+    occupation;
+    message;
+
+    constructor(name, age, occupation, message){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.message = message;
+    }
+
+    displayInfo(){
+        console.log("Name: "+ this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: " + this.occupation);
+        console.log("Message: " + this.message);
+        console.log("-----------------------")
+    }
+
+    showName(){
+        console.log(this.name);
+    }
+
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const person1 = new Person("Coco Martin",24,"Masahista","Ser, tapos na po");
+const person2 = new Person("Victor Magtanggol",24,"Superhero","Arjoooooooo");
+
+person1.displayInfo();
+person2.displayInfo();
